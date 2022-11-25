2d862f27e8e7bd0b036de86c30cc91e98b1f276e
Papallism <papallis.m@unic.ac.cy>
2022-11-25 19:17:15 +0200

    added hide modal functionality

--- src/UI/Modal.js
+++ src/UI/Modal.js
@@ -8,21 +8,28 @@ export class Modal {
   show() {
     if ('content' in document.createElement('template')) {
       const modalInnerElements = document.importNode(this.modalTemplateElement.content, true)
-      const modalElement = modalInnerElements.querySelector('.modal')
-      const backdropElement = modalInnerElements.querySelector('.backdrop')
+      
+      this.modalElement = modalInnerElements.querySelector('.modal')
+      this.backdropElement = modalInnerElements.querySelector('.backdrop')
 
       const contentElement = document.importNode(this.contentTemplateElement.content, true)
 
-      modalElement.appendChild(contentElement)
+      this.modalElement.appendChild(contentElement)
 
-      document.body.insertAdjacentElement('afterbegin', modalElement)
-      document.body.insertAdjacentElement('afterbegin', backdropElement)
+      document.body.insertAdjacentElement('afterbegin', this.modalElement)
+      document.body.insertAdjacentElement('afterbegin', this.backdropElement)
     } else {
       alert(this.fallbackText)
     }
   }
 
   hide() {
+    if (this.modalElement) {
+      document.body.removeChild(this.modalElement)
+      document.body.removeChild(this.backdropElement)
 
+      this.modalElement = null
+      this.backdropElement = null
+    }
   }
 }
